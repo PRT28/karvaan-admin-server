bb5dee35059274eb35919d1901b8b487af03518d
Prithvi Raj Tiwari <prt@Prithvis-MacBook-Air.local>
2025-12-06 11:10:11 +0530

    next

--- utils/s3.ts
+++ utils/s3.ts
@@ -3,10 +3,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 // Initialize S3 Client
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION || 'eu-north-1',
+  region: process.env.AWS_REGION || '',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'AKIAXGLE2RBDAPPUDPTE',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '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',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
   },
 });
 
